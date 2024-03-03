baf1aed8d54cd3292b68b8aebe64607e96dce2b6
Kultrol <physicsKev@outlook.com>
2024-03-02 19:36:10 -0500

    Improved snippet structure

--- init.lua
+++ init.lua
@@ -14,4 +14,26 @@ vim.g.vimtex_view_general_viewer = 'okular'
 vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
 vim.g.vimtex_compiler_method = 'latexmk'
 
+--Theme
+vim.cmd[[colorscheme tokyonight]]
+
+local function autoChangeTheme()
+    local hour = os.date("*t").hour
+    local lightTheme = 'tokyonight-day'
+    local darkTheme = 'tokyonight'
+
+    if hour >= 7 and hour < 18 then
+        vim.cmd("colorscheme " .. lightTheme)
+    else
+        vim.cmd("colorscheme " .. darkTheme)
+    end
+end
+
+autoChangeTheme()
+
+local timer = vim.loop.new_timer()
+timer:start(0, 60000 * 60, vim.schedule_wrap(function()
+    autoChangeTheme()
+end))
+
 
